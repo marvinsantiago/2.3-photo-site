2ef7811a1b2db7619994c563b3b8b8d9649eb88a
Marvin Santiago <marvin.santiago@ymail.com>
2015-07-09 16:25:19 -0500

    Corrected Brocfile

--- Brocfile.js
+++ Brocfile.js
@@ -1,7 +1,12 @@
 var merge = require('broccoli-merge-trees');
 var compileSass = require('broccoli-sass');
 
-var includePaths = [ 'assets', 'bower_components', 'bower_components/fontawesome/scss', bower_components/bourbon/app/assets/stylesheets', 'bower_components/neat/app/assets/stylesheets'];
+var includePaths = ([
+'assets',
+'bower_components',
+'bower_components/fontawesome/scss',
+'bower_components/bourbon/app/assets/stylesheets',
+'bower_components/neat/app/assets/stylesheets']);
 
 var appCss = compileSass(includePaths, 'app.scss', 'app.css');
 
